c4a11025d858a7fa6f7dee0baa258a327d18b727
esalaz415 <esalaz415@gmail.com>
2018-03-29 14:15:34 -0700

    session handeling in react w/ higher order components

---
+++ src/components/withAuthentication.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { firebase } from '../firebase';
+
+const withAuthentication = (Component) => {
+  class withAuthentication extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        authUser: null,
+      };
+    }
+
+    getChildContext() {
+      return {
+        authUser: this.state.authUser,
+      };
+    }
+
+    componentDidMount() {
+      firebase.auth.onAuthStateChanged(authUser => {
+        authUser
+          ? this.setState(() => ({ authUser }))
+          : this.setState(() => ({ authUser: null }));
+      });
+    }
+
+    render() {
+      return (
+        <Component />
+      );
+    }
+  }
+
+  withAuthentication.childContextTypes = {
+    authUser: PropTypes.object,
+  };
+
+  return withAuthentication;
+}
+
+export default withAuthentication;
