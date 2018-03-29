0aa299f4050abbaab8f5e817683c3bf55de635b5
mvargasvega <martin@digitalnest.org>
2018-03-29 14:24:32 -0700

    Made the changes to App.js, Navigation.js, and withAuthentication.js, to make session handling with Higher Order Components

--- src/components/App.js
+++ src/components/App.js
@@ -13,58 +13,24 @@ import HomePage from './Home';
 import AccountPage from './Account';
 
 import * as routes from '../constants/routes';
-import {firebase} from '../firebase';
-class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      authUser:null,
-    };
-  }
-  componentDidMount() {
-  firebase.auth.onAuthStateChanged(authUser => {
-    authUser
-      ? this.setState(() => ({ authUser }))
-      : this.setState(() => ({ authUser: null }));
-    });
-  }
+import withAuthentication from './withAuthentication';
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navigation authUser={this.state.authUser} />
 
-          <hr/>
-          <Route
-            exact path={routes.LANDING}
-            component={() => <LandingPage />}
-          />
-          <Route
-            exact path={routes.SIGN_UP}
-            component={() => <SignUpPage />}
-          />
-          <Route
-            exact path={routes.SIGN_IN}
-            component={() => <SignInPage />}
-          />
-          <Route
-            exact path={routes.PASSWORD_FORGET}
-            component={() => <PasswordForgetPage />}
-          />
-          <Route
-            exact path={routes.HOME}
-            component={() => <HomePage />}
-          />
-          <Route
-            exact path={routes.ACCOUNT}
-            component={() => <AccountPage />}
-          />
-        </div>
-      </Router>
-    )
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <Navigation />
 
-export default App;
+      <hr/>
+
+      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+      <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+      <Route exact path={routes.HOME} component={() => <HomePage />} />
+      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+    </div>
+  </Router>
+
+  export default withAuthentication(App);
