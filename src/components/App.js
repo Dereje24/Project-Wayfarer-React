e763147c540608c991abc7918503d311ffe67993
mvargasvega <martin@digitalnest.org>
2018-03-29 11:45:47 -0700

    Authentication is complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router,
   Route,
  } from 'react-router-dom';
@@ -12,13 +12,33 @@ import HomePage from './Home';
 import AccountPage from './Account';
 
 import * as routes from '../constants/routes';
+import { firebase } from '../firebase';
 
-const App = () =>
-  <Router>
-    <div>
-        <Navigation />
 
-        <hr/>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null,
+    };
+  }
+
+  componentDidMount() {
+   firebase.auth.onAuthStateChanged(authUser => {
+     authUser
+       ? this.setState(() => ({ authUser }))
+       : this.setState(() => ({ authUser: null }));
+   });
+ }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation authUser={this.state.authUser} />
+
+          <hr/>
 
         <Route
           exact path={routes.LANDING}
@@ -46,5 +66,7 @@ const App = () =>
         />
       </div>
   </Router>
-
+    );
+  }
+}
 export default App;
